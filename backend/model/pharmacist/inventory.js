94484c90c02093ee85ff20512ea8a34f79e3a768
Anjali V B <anjalivbbala@gmail.com>
2025-07-31 13:36:56 +0530

    updated labtec and pharmacist

--- backend/model/pharmacist/inventory.js
+++ backend/model/pharmacist/inventory.js
@@ -1,13 +1,16 @@
-const mongoose = require('mongoose');
-const AutoIncrement = require('mongoose-sequence')(mongoose);
+const mongoose = require("mongoose");
+const AutoIncrement = require("mongoose-sequence")(mongoose);
 
-const InventorySchema = new mongoose.Schema({
-  inventoryId: { type: Number, unique: true },
-  medicineId: { type: Number, ref: 'Medicine' },
-  quantity: Number,
-  expiryDate: Date,
-  lowStockFlag: { type: Boolean, default: false }
-}, { timestamps: true, autoIndex: true });
+const InventorySchema = new mongoose.Schema(
+  {
+    inventoryId: { type: Number, unique: true },
+    medicineId: { type: Number, ref: "Medicine" },
+    quantity: Number,
+    expiryDate: Date,
+    lowStockFlag: { type: Boolean, default: false },
+  },
+  { timestamps: true, autoIndex: true, _id: false, versionKey: false }
+);
 
-InventorySchema.plugin(AutoIncrement, { inc_field: 'inventoryId' });
-module.exports = mongoose.model('Inventory', InventorySchema);
+InventorySchema.plugin(AutoIncrement, { inc_field: "inventoryId" });
+module.exports = mongoose.model("Inventory", InventorySchema);
