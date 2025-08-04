bb076efa930eb4a44a9926708d1d751e65f4cc88
rejul <rejul.reghu@gmail.com>
2025-08-04 12:42:40 +0530

    minor

--- backend/model/admin/doctor.js
+++ backend/model/admin/doctor.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const DoctorSchema = new mongoose.Schema({
-  doctorId: { type: Number, unique: true }, // This is the field being auto-incremented
-  staffId: { type: Number, ref: 'Staff' },
-  specializationId: { type: Number, ref: 'Specialization' },
-  qualifications: String,
+  doctorId: { type: Number, unique: true}, // This is the field being auto-incremented
+  staffId: { type: Number, ref: 'Staff', required: true },
+  specializationId: { type: Number, ref: 'Specialization', required: true },
+  qualifications: { type: String, required: true },
   isActive: { type: Boolean, default: true }
 }, {
   timestamps: true,
